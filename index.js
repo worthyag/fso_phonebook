def02240399397362ae6122b022cdec24aed57fe
Worthy <worthyintech@gmail.com>
2024-09-11 17:01:52 +0100

    Completed exercises 3.1and 3.2.

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+];
 
 app.get("/", (request, response) => {
   response.send("<h1>Welcome home!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(p => p.id === id);
+  response.json(person);
+});
+
+app.get("/info", (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${(new Date())}</p>
+  `);
 });
 
 const PORT = 3001
