ada585d28e6fe99e28f22b01d5329574d25f51ad
Worthy <worthyintech@gmail.com>
2024-09-11 17:07:37 +0100

    Completed exercise 3.4.

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.get("/api/persons/:id", (request, response) => {
     response.status(404).send("Person does not exist!");
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter(p => p.id !== id);
+  response.status(204).end();
+});
+
 app.get("/info", (request, response) => {
   response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
