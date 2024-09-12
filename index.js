f17e8ff722be013e76097654118ee80449d1044a
Worthy <worthyintech@gmail.com>
2024-09-12 11:57:28 +0100

    Completed exercise 3.8.

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    (tokens.method(req, res) === "POST") 
+      ? JSON.stringify(req.body) : ""
+  ].join(' ');
+}));
 
 let persons = [
   { 
