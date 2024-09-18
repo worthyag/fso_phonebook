4d663a8269bf01c160ffb2d6c56c0703cc2c6e91
Worthy <worthyintech@gmail.com>
2024-09-18 09:29:03 +0100

    Completed exercise 3.9.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(morgan((tokens, req, res) => {
   return [
@@ -102,7 +104,7 @@ app.get("/info", (request, response) => {
   `);
 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}.`);
